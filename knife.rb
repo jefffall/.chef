998e9ac82f3c9a6c4bd540e9f727662c5d7ef177
Jeffrey Fall <jeffrey_fall@vp0102wa-dhcp171.apple.com>
2017-11-03 17:33:23 -0700

    First commit

---
+++ knife.rb
@@ -0,0 +1,9 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "jeffrey_fall"
+validation_client_name   "chef-validator"
+client_key               "/Users/jeffrey_fall/.chef/jeffrey_fall.pem"
+validation_key		"/Users/jeffrey_fall/.chef/chef_validator.pem"
+chef_server_url          "https://chefserver/organizations/myorg"
+cookbook_path            ["#{current_dir}/../cookbooks"]
