f1a60ba0de9215fb91d1820b45e5f0a4acb9df2e
Jeffrey Fall <jeffrey_fall@vp0102wa-dhcp171.apple.com>
2017-11-07 10:11:30 -0800

    fixed knife.rb with cookbook address of chef-repo/cookbooks

--- knife.rb
+++ knife.rb
@@ -6,4 +6,5 @@ validation_client_name   "chef-validator"
 client_key               "/Users/jeffrey_fall/.chef/jeffrey_fall.pem"
 validation_key		"/Users/jeffrey_fall/.chef/chef_validator.pem"
 chef_server_url          "https://chefserver/organizations/myorg"
-cookbook_path            ["#{current_dir}/../cookbooks"]
+#cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            "/Users/jeffrey_fall/chef-repo/cookbooks" 
